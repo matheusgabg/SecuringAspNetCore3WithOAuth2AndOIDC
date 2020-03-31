010fc2a5d029ebaafa627d7ab42ef0ca1aac5441
goncmat <goncmat@USM1GQN7S2>
2020-03-31 11:57:15 -0400

    fixed error on sub Id

--- Starter files/src/IdentityServer.IDP/Quickstart/TestUsers.cs
+++ Starter files/src/IdentityServer.IDP/Quickstart/TestUsers.cs
@@ -23,12 +23,14 @@ namespace IdentityServer.IDP
                     new Claim("given_name", "Frank"),
                     new Claim("family_name", "Underwood"),
                     new Claim("address","Main Road 1"),
-                    new Claim("role","FreeUser")
+                    new Claim("role","FreeUser"),
+                    new Claim("subscriptionlevel","FreeUser"),
+                    new Claim("country","nl"),
                 }
             },
             new TestUser
             {
-                SubjectId = "b7539694-97e7-4dfe-84da-b425e1ff5c7",
+                SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
                 Username = "Claire",
                 Password = "password",
                 Claims = new List<Claim>
@@ -36,7 +38,9 @@ namespace IdentityServer.IDP
                     new Claim("given_name", "Claire"),
                     new Claim("family_name", "Underwood"),
                     new Claim("address","Big Street 2"),
-                    new Claim("role","PayingUser")
+                    new Claim("role","PayingUser"),
+                    new Claim("subscriptionlevel","PayingUser"),
+                    new Claim("country","be"),
                 }
             }
         };
